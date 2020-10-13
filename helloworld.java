7c6f408e1485799359cae6bc52d38d739fbf0321
naziauser <72778692+naziauser@users.noreply.github.com>
2020-10-13 05:37:31 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld
 {
 	public static void main(String[] args) {
-		System.out.println("This is my first  latest project");
+		System.out.println("This is my the first latest project");
 	}
 }
